840680b4502502b63e704fef570c081f53364bb0
Samir Kumar <46542096+krsamir@users.noreply.github.com>
2022-12-08 17:00:39 +0530

    Integration of react hook form

--- src/Components/Authentication/Login.jsx
+++ src/Components/Authentication/Login.jsx
@@ -1,18 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 import "./Login.css";
 import avtar from "../../Assets/avatar.png";
 import { useLogin } from "./Hooks/useLoginHook";
 function Login() {
-  const [data, setData] = useState({
-    username: "admin",
-    password: "admin",
+  const FIELD_NAMES = Object.freeze({
+    USERNAME: "username",
+    PASSWORD: "password",
   });
   const { handleLogin } = useLogin();
-  const handleChange = ({ target: { name, value } }) => {
-    const parsedData = { ...data };
-    parsedData[name] = value;
-    setData(parsedData);
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      [FIELD_NAMES.USERNAME]: "",
+      [FIELD_NAMES.PASSWORD]: "",
+    },
+  });
+  const onSubmit = (data) => handleLogin(data);
+
   return (
     <div className="login-app">
       <div className="login">
@@ -20,35 +28,42 @@ function Login() {
           <img src={avtar} alt="Avtar" />
         </div>
         <h2>Login</h2>
-        <h3>Welcome Back</h3>
+        <h3>Welcome</h3>
 
-        <form className="login-form">
+        <form
+          className="login-form"
+          onSubmit={handleSubmit(onSubmit)}
+          autoComplete="off"
+        >
           <div className="textbox">
             <input
+              {...register(FIELD_NAMES.USERNAME, {
+                required: { value: true, message: "Please Enter Username" },
+              })}
               type="text"
               placeholder="Username"
-              autoComplete="false"
-              name="username"
-              onChange={handleChange}
-              value={data.username}
             />
-            {/* <span className="material-symbols-outlined"> account_circle </span> */}
           </div>
+          {errors[FIELD_NAMES.USERNAME]?.type === "required" && (
+            <div role="alert" className="error-text">
+              {errors[FIELD_NAMES.USERNAME]?.message}
+            </div>
+          )}
           <div className="textbox">
             <input
+              {...register(FIELD_NAMES.PASSWORD, {
+                required: { value: true, message: "Please Enter Password" },
+              })}
               type="password"
               placeholder="Password"
-              autoComplete="false"
-              name="password"
-              onChange={handleChange}
-              value={data.password}
             />
-            {/* <span className="material-symbols-outlined"> lock </span> */}
           </div>
-          <button type="button" onClick={() => handleLogin(data)}>
-            LOGIN
-          </button>
-          <span>Forgot your credentials?</span>
+          {errors[FIELD_NAMES.PASSWORD] && (
+            <div role="alert" className="error-text">
+              {errors[FIELD_NAMES.PASSWORD]?.message}
+            </div>
+          )}
+          <button type="submit">LOGIN</button>
         </form>
       </div>
     </div>
