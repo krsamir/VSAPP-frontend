8797d8343a5fe1640705fc4c5be6d7392a8f632e
Samir Kumar <46542096+krsamir@users.noreply.github.com>
2022-12-08 22:23:19 +0530

    Refactoring Routes

--- src/Routes1.jsx
+++ src/Routes1.jsx
@@ -1,69 +1,52 @@
-import Loader from "./Components/Common/Loader/Loader";
+import Loader from "./Components/Common/Loader";
 import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import PrivateRoute from "./Components/Authentication/PrivateRoute";
+import ErrorBoundryRoutes from "./Components/Error/Error-boundary-routes";
 import { ROUTES_PATH } from "./Utilities/Routes-config";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const PageNotFound = lazy(() => import("./Components/Error/page-not-found"));
 const Login = lazy(() => import("./Components/Authentication/Login"));
 const Home = lazy(() => import("./Components/Common/Home"));
-
-const router = createBrowserRouter([
-  {
-    path: ROUTES_PATH.LOGIN,
-    element: <Login />,
-  },
-  {
-    path: ROUTES_PATH.SUPER_ADMIN.ROOT,
-    element: (
-      <PrivateRoute>
-        <Home />
-      </PrivateRoute>
-    ),
-    children: [
-      {
-        path: "*",
-        element: <PageNotFound />,
-      },
-    ],
-  },
-  {
-    path: ROUTES_PATH.ADMIN.ROOT,
-    element: (
-      <PrivateRoute>
-        <Home />
-      </PrivateRoute>
-    ),
-    children: [
-      {
-        path: "*",
-        element: <PageNotFound />,
-      },
-    ],
-  },
-  {
-    path: ROUTES_PATH.USER.ROOT,
-    element: (
-      <PrivateRoute>
-        <Home />
-      </PrivateRoute>
-    ),
-    children: [
-      {
-        path: "*",
-        element: <PageNotFound />,
-      },
-    ],
-  },
-]);
-
 function Routes() {
   return (
-    <>
-      <Suspense fallback={<Loader />}>
-        <RouterProvider router={router} />
-      </Suspense>
-    </>
+    <div>
+      <BrowserRouter>
+        <Suspense fallback={<Loader />}>
+          <ErrorBoundryRoutes>
+            <Route
+              path={ROUTES_PATH.SUPER_ADMIN.ROOT}
+              index
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            ></Route>
+            <Route
+              path={ROUTES_PATH.ADMIN.ROOT}
+              index
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            ></Route>
+            <Route
+              path={ROUTES_PATH.USER.ROOT}
+              index
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            ></Route>
+            <Route path={ROUTES_PATH.LOGIN} element={<Login />} />
+            <Route path="*" element={<PageNotFound />} />
+          </ErrorBoundryRoutes>
+        </Suspense>
+      </BrowserRouter>
+    </div>
   );
 }
 
